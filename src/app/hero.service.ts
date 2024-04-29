b1466c62bf6a38abca080b222679a1dfea376e33
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-29 17:48:20 +0800

    create function getHeroes()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }

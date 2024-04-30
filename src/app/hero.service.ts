98c7b2bf6d7310d5160b09485a53396095f92e83
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-30 17:02:59 +0800

    import MessageService into HeroService and send a message when heroes are fetched

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,9 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageSerivce.add('HeroService: fetched heroes');
     return heroes;
   }
+
+  constructor(private messageSerivce: MessageService){}
 }
